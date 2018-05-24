bbfa417c2e54b6566133e71fc6315877a4af64d6
Xiaowei Yue <xiaowei.isye@gmail.com>
2018-05-24 13:39:10 -0400

    Add files via upload

--- PMD_main_v0_2.m
+++ PMD_main_v0_2.m
@@ -12,15 +12,19 @@ addpath(genpath('.\results'));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 1. Simulation
 %% 1.1 Generate simulation dataset based on ideal Raman curve
+disp('Generate simulation dataset...')
 run PMD_1_1_gen_sim_data_v1.m
 
 %% 1.2 Wavelet-based Penalized Mixed Effects Model
+disp('Run the Penalized Mixed Effects Decomposition...')
 run PMD_1_2_run_PMD_v1.m
 
 %% 1.3 Sensitivity Analysis of the bounds and the penalized parameter
+disp('Run the Sensitivity Analysis of the bounds and the penalized parameter...')
 run PMD_1_3_sensitivity_analysis_v2.m 
 
 %% 1.4 Benchmark 1: Linear Mixed Effects Model (LMM) based on Spline
+
 run PMD_1_4_benchmark1_LMM_v1.m
 
 %% 1.5 Benchmark 2: Smooth Sparse Decomposition (SSD)
@@ -32,18 +36,23 @@ run PMD_1_4_benchmark1_LMM_v1.m
 
 %% 1.6 Show the results of Simulation
 % load all the results above from 'result_v1.mat'
+disp('Show the result...')
 run PMD_1_6_show_result_v1.m
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 2.Real Data Analysis
 %% 2.1 introduce real data
 clc;clear;close all;
+disp('Introduce the real data...')
 run PMD_2_1_intro_real_data_v1.m
 
 %% 2.2 PMD Analysis
+disp('Run the PMD method...')
 run PMD_2_2_run_PMD_v1.m
 
 %% 2.3 show results
+disp('Show the results...')
 run PMD_2_3_show_result_v1.m
 
+disp('Completed.')
 
